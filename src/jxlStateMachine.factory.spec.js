bdf8acb2e3eb7af56827a9ed162070ba53e7db4b
JesterXL <jesse@jessewarden.com>
2014-11-30 20:33:17 -0500

    lol, old tests, man, need some more update to date ones. Forgot all of the Chai methods on the plane... need internets. Digging around in node_modules is miserable.

--- src/jxlStateMachine.factory.spec.js
+++ src/jxlStateMachine.factory.spec.js
@@ -1,4 +1,4 @@
-describe('logger', function()
+describe('jxlStateMachine Factory Spec', function()
 {
     var jxlStateMachine = null;
 
@@ -26,5 +26,21 @@ describe('logger', function()
     {
         assert.isNotNull(jxlStateMachine);
     });
+
+    // it('should give an instance', function()
+    // {
+    //     console.log("jxlStateMachine:", jxlStateMachine);
+    //     console.log("new jxlStateMachine:", new jxlStateMachine());
+    //     var instance = new jxlStateMachine();
+    //     assert.isNotNull(instance);
+    // });
+
+    // it('instances should be unique', function()
+    // {
+    //     var instanceA = new jxlStateMachine();
+    //     var instanceB = new jxlStateMachine();
+    //     expect(instanceA == instanceB).to.equal(false);
+    //     expect(instanceA === instanceB).to.equal(false);
+    // });
 
 });
